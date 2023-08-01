af606aecc0e75ec94ab1f24ab49f8942a735bd1d
zineb790 <nohad91@hotmail.fr>
2023-07-31 23:59:20 -0400

    first try

---
+++ hiking-trail/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import TrailComponent from "./components/TrailComponent" 
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>welcome my biking trail locator</h1>
+      <h2>test msg</h2>
+      
+      
+    </div>
+  );
+}; 
+
+export default App;
